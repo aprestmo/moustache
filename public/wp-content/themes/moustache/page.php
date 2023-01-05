a476863ae4e199fd2c60d3e871581d2b26845538
Alexander Prestmo <aprestmo@gmail.com>
2023-01-05 12:15:44 +0100

    Add flow to pages

--- public/wp-content/themes/moustache/page.php
+++ public/wp-content/themes/moustache/page.php
@@ -8,7 +8,7 @@ while (have_posts()) :
   <div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
     <div class="o-grid u-text-center">
       <div class="o-grid__item u-1/1 u-2/3@sm">
-        <section class="o-section-md">
+        <section class="o-section-md u-flow">
           <h1><?php the_title(); ?></h1>
           <?php the_content(); ?>
         </section>
