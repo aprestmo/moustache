9e730e423b000c8abb944a5e7a0c8e7ce46dc56f
Alexander Prestmo <aprestmo@gmail.com>
2023-05-11 12:31:06 +0200

    Add pitches page

---
+++ public/wp-content/themes/moustache/archive-club.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Clubs overview template
+ * @package Moustache 5
+ */
+
+get_header(); ?>
+
+<div class="mou-site-wrap mou-site-wrap--padding u-soft-top-md u-push-bottom-lg">
+	<?php
+	$posts = get_posts(
+		array(
+			'numberposts' => -1,
+			'post_type'   => 'club',
+			'orderby'     => 'title',
+			'order'       => 'ASC',
+		)
+	);
+
+	if ($posts) :
+		?>
+
+		<h2><?php esc_html_e('Klubboversikt', 'moustache'); ?></h2>
+		<ul class="u-soft-top-md">
+			<?php foreach ($posts as $post) : ?>
+				<li class="o-grid__item u-1/2 u-1/5@md">
+					<?php echo get_the_title($post->ID); ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+
+	<?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
--- public/wp-content/themes/moustache/archive-pitch.php
+++ public/wp-content/themes/moustache/archive-pitch.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Pitch overview template
  *
@@ -10,60 +9,46 @@ get_header(); ?>
 
 <?php
 $posts = get_posts(
-  array(
-    'numberposts' => -1,
-    'post_type'   => 'pitch',
-    'orderby'     => 'title',
-    'order'       => 'ASC',
-  )
+	array(
+		'numberposts' => -1,
+		'post_type' => 'pitch',
+		'orderby' => 'title',
+		'order' => 'ASC',
+	)
 );
 
-if ($posts) {
+if ($posts) { ?>
+<section class="mou-site-wrap mou-site-wrap--padding u-soft-top-md u-push-bottom-lg">
+	<?php foreach ($posts as $post)	: ?>
+		<h1><?php the_title(); ?></h1>
+		<?php if (get_field('surface')) {
+			$field = get_field_object('surface');
+			$value = get_field('surface');
+			$label = $field['choices'][$value]; ?>
+			<dl>
+				<dt><?php esc_html_e('Surface', 'moustache'); ?></dt>
+				<dd><?php esc_html_e($label); ?></dd>
+			</dl>
+		<?php } ?>
 
-  foreach ($posts as $post) {
-?>
+		<?php if (get_field('image')) :
+			$image = get_field('image');
+		?>
+			<img src="<?php esc_attr_e($image); ?>" alt="">
+		<?php endif; ?>
 
-
-<?php
-$location = get_field('address');
-if( $location ): ?>
-    <div class="acf-map" data-zoom="16">
-        <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
-    </div>
-<?php endif; ?>
-
-
-    <h1><?php the_title(); ?></h1>
-
-    <?php if (get_field('surface')) : ?>
-      <?php
-      $field = get_field_object('surface');
-      $value = get_field('surface');
-      $label = $field['choices'][$value];
-      ?>
-      <dl>
-        <dt><?php esc_html_e('Surface', 'moustache'); ?></dt>
-        <dd><?php esc_html_e($label); ?></dd>
-      </dl>
-
-    <?php endif; ?>
-
-    <?php if (get_field('image')) : ?>
-      <?php
-      $image = get_field('image');
-      ?>
-      <img src="<?php esc_attr_e($image); ?>" alt="">
-
-    <?php endif; ?>
-
-    <?php if (get_field('address')) : ?>
-      <?php $map = get_field('address'); ?>
-      <?php /*<pre><?php var_dump($map); ?></pre> */ ?>
-      <?php echo $map['address']; ?>
-    <?php endif; ?>
-
-  <?php } ?>
-
+		<?php if (get_field('address')) {
+			$map = get_field('address');
+			echo $map['address'];
+		}
+		$location = get_field('address');
+		if ($location) : ?>
+			<div class="acf-map" data-zoom="16">
+				<div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>">
+			</div>
+		<?php endif; ?>
+	<?php endforeach; ?>
 <?php } ?>
+</section>
 
 <?php get_footer(); ?>
