e28ac4b5739dad33e20e9137e6204228e33593f0
Alexander Prestmo <aprestmo@gmail.com>
2023-06-05 23:35:07 +0200

    Add pitch pages

--- public/wp-content/themes/moustache/single-pitch.php
+++ public/wp-content/themes/moustache/single-pitch.php
@@ -1,40 +1,49 @@
-<?php get_header(); ?>
-
 <?php
-while (have_posts()) :
-  the_post();
-?>
+/**
+ * Single pitch template
+ *
+ * @since 6.0.0
+ */
+get_header(); ?>
 
-  <h1><?php the_title(); ?></h1>
+<?php while (have_posts()) : the_post(); ?>
 
-  <?php
-  $image = get_field('image');
-  if (!empty($image)) :
-  ?>
-    <img src="<?php echo $image['sizes']['large']; ?>" alt="
-						<?php
-            _e('Image from ', 'moustache');
-            the_title();
-            ?>
-">
-  <?php endif; ?>
+<div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
+	<div class="o-grid u-text-center">
+			<div class="o-grid__item u-1/1">
+				<section class="o-section-md">
+					<h1><?php the_title(); ?></h1>
+					<?php
+						$image = get_field('image');
+						$size = 'medium';
+					if ($image) : ?>
+					<img src="<?php echo $image['sizes'][$size]; ?>" alt="<?php _e('Image from ', 'moustache'); the_title(); ?>">
+					<?php endif; ?>
 
-  <?php
-  $field = get_field_object('surface');
-  $value = get_field('surface');
-  $label = $field['choices'][$value];
+					<?php
+						$map = get_field('address');
+						$address = $map['address'];
+					?>
+					<div class="acf-map" data-zoom="17">
+						<div class="marker" data-lat="<?php echo esc_attr($map['lat']); ?>" data-lng="<?php echo esc_attr($map['lng']); ?>"></div>
+					</div>
+					<p><?php esc_html_e($address); ?></p>
 
-  _e('Surface', 'moustache');
-  echo $label;
-  ?>
-
-  <?php
-  // Sjekk OPPguiden
-  $map     = get_field('address');
-  $address = $map['address'];
-
-  echo $address;
-  ?>
+					<div>
+					<?php
+						$field = get_field_object('surface');
+						$value = get_field('surface');
+					?>
+						<dl>
+							<dt><?php esc_html_e('Surface', 'moustache'); ?></dt>
+							<dd><?php esc_html_e($field['choices'][$value]); ?></dd>
+						</dl>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php endwhile; ?>
 
