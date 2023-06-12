45e1fa7625b6ba2272623130ef49fda42836040f
Alexander Prestmo <aprestmo@gmail.com>
2023-06-12 18:44:58 +0200

    Add fields

--- public/wp-content/themes/moustache/page-klubbinfo.php
+++ public/wp-content/themes/moustache/page-klubbinfo.php
@@ -5,19 +5,35 @@ get_header(); ?>
 
 <?php
 while (have_posts()) :
-  the_post();
+	the_post();
 ?>
 
-  <div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
-    <div class="o-grid u-text-center">
-      <div class="o-grid__item u-1/1 u-2/3@sm">
-        <section class="o-section-md u-flow">
-          <h1><?php the_title(); ?></h1>
-          <?php the_content(); ?>
-        </section>
-      </div>
-    </div>
-  </div>
+	<div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
+		<div class="o-grid u-text-center">
+			<div class="o-grid__item u-1/1 u-2/3@sm">
+				<section class="o-section-md u-flow">
+					<h1><?php the_title(); ?></h1>
+					<dl>
+						<dt>Stitet:</dt>
+						<dd>
+							<?php the_field('founded'); ?>
+						</dd>
+						<dt>Postadresse:</dt>
+						<dd></dd>
+						<dt>Kontonummer:</dt>
+						<dd>
+							<?php the_field('bank_account'); ?>
+						</dd>
+						<dt>Drakter:</dt>
+						<dd>
+							<?php the_field('kit'); ?>
+						</dd>
+					</dl>
+					<?php the_content(); ?>
+				</section>
+			</div>
+		</div>
+	</div>
 
 <?php endwhile; ?>
 
