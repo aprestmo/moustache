fc9390ed0b437a6b6cf3f53c63c8f9ed1295f9df
Alexander Prestmo <aprestmo@gmail.com>
2023-06-13 09:02:17 +0200

    Add Gullbart winners

--- public/wp-content/themes/moustache/page-klubbinfo.php
+++ public/wp-content/themes/moustache/page-klubbinfo.php
@@ -14,42 +14,44 @@ while (have_posts()) :
 				<section class="o-section-md u-flow">
 					<h1><?php the_title(); ?></h1>
 					<dl>
-						<dt>Stitet:</dt>
+						<dt><?php esc_html_e('Founded', 'moustache'); ?>:</dt>
 						<dd>
-							<?php the_field('founded'); ?>
+							<?php esc_html_e(get_field('founded')); ?>
 						</dd>
-						<dt>Postadresse:</dt>
-						<dd></dd>
-						<dt>Kontonummer:</dt>
+						<dt><?php esc_html_e('Address', 'moustache'); ?>:</dt>
+						<?php
+							if ( have_rows('contact_info') ) :
+								while ( have_rows('contact_info') ) : the_row();
+							?>
+								<dd><?php esc_html_e(get_sub_field('name')); ?><br><?php esc_html_e(get_sub_field('address')); ?> <?php esc_html_e(get_sub_field('zip')); ?><br><?php esc_html_e(get_sub_field('place')); ?></dd>
+						<?php endwhile; endif; ?>
+						<dt><?php esc_html_e('Account number', 'moustache'); ?>:</dt>
 						<dd>
-							<?php the_field('bank_account'); ?>
+							<?php esc_html_e(get_field('bank_account')); ?>
 						</dd>
-						<dt>Drakter:</dt>
+						<dt><?php esc_html_e('Kit', 'moustache'); ?>:</dt>
 						<dd>
-							<?php the_field('kit'); ?>
+							<?php esc_html_e(get_field('kit')); ?>
 						</dd>
 					</dl>
 					<?php the_content(); ?>
 					<?php
-					// Check rows existexists.
-					if( have_rows('gullbart') ):
+						if ( have_rows('gullbart') ):
 							echo '<dl>';
-							// Loop through rows.
-							while( have_rows('gullbart') ) : the_row();
+							while ( have_rows('gullbart') ) : the_row();
 
-									// Load sub field value.
 									$year = get_sub_field('gullbart_year');
-									$winner = get_sub_field('gullbart_winner');
-									// Do something...
-									echo '<dt>' . $year . '</dt>';
-									echo '<dd>' . $winner . '</dd>';
-							// End loop.
+									$winners = get_sub_field('gullbart_winner');
+
+									if ( $winners ) {
+										foreach ($winners as $winner) {
+											echo '<dt>' . $year . '</dt>';
+											echo '<dd><a href="/spiller/' . $winner->post_name . '/">'. $winner->post_title . '</a></dd>';
+										}
+									}
 							endwhile;
 							echo '</dl>';
-					// No value.
-					else :
-							// Do something...
-					endif;
+						endif;
 					?>
 				</section>
 			</div>
