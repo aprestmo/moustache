afc5d6ac11c81ba9ef0e38f91da92067b53676f2
Alexander Prestmo <aprestmo@gmail.com>
2023-06-14 09:39:02 +0200

    Move songs to partial

--- public/wp-content/themes/moustache/page.php
+++ public/wp-content/themes/moustache/page.php
@@ -1,21 +1,17 @@
 <?php get_header(); ?>
 
-<?php
-while (have_posts()) :
-  the_post();
-?>
-
-  <div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
-    <div class="o-grid u-text-center">
-      <div class="o-grid__item u-1/1 u-2/3@sm">
-        <section class="o-section-md u-flow">
-          <h1><?php the_title(); ?></h1>
-          <?php the_content(); ?>
-        </section>
-      </div>
-    </div>
-  </div>
-
+<?php while (have_posts()) : the_post(); ?>
+<div class="mou-site-wrap mou-site-wrap--padding wysiwyg">
+	<div class="o-grid u-text-center">
+		<div class="o-grid__item u-1/1 u-2/3@sm">
+			<section class="o-section-md u-flow">
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+				<?php get_template_part( 'template-parts/songs', 'page' ); ?>
+			</section>
+		</div>
+	</div>
+</div>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
---
+++ public/wp-content/themes/moustache/template-parts/songs.php
@@ -0,0 +1,19 @@
+<?php
+	if ( have_rows('music') ):
+		echo '<div>';
+		while ( have_rows('music') ) : the_row();
+			echo '<figure>';
+				$title = get_sub_field('title');
+				$audio = get_sub_field('audio_file');
+				$lyrics = get_sub_field('lyrics_file');
+
+				echo '<figcaption>' . $title . '</figcaption>';
+				echo '<audio controls src="' . $audio .'" style="inline-size: 100%">';
+					echo '<a href="' . $audio . '">Last ned' . $title . '</a>';
+				echo '</audio>';
+				echo '<p><a href="' . $lyrics . '">Last ned teksten til «' . $title . '»</a></p>';
+			echo '</figure>';
+		endwhile;
+		echo '</div>';
+	endif;
+?>
