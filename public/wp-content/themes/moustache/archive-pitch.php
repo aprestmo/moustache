eb768841ff8613ad10c67a9f22e2f9ef36cf232e
Alexander Prestmo <aprestmo@gmail.com>
2023-06-14 10:40:04 +0200

    Update pitches overview

--- public/wp-content/themes/moustache/archive-pitch.php
+++ public/wp-content/themes/moustache/archive-pitch.php
@@ -2,57 +2,36 @@
 /**
  * Pitch overview template
  *
- * TODO: Design and map
+ * @package Moustache 5
  */
+
 get_header(); ?>
 
+<div class="mou-site-wrap mou-site-wrap--padding wysiwyg u-soft-top-md u-push-bottom-lg">
+	<?php
+	$posts = get_posts(
+		array(
+			'numberposts' => -1,
+			'post_type'   => 'pitch',
+			'orderby'     => 'title',
+			'order'       => 'ASC',
+		)
+	);
 
-<?php
-$posts = get_posts(
-	array(
-		'numberposts' => -1,
-		'post_type' => 'pitch',
-		'orderby' => 'title',
-		'order' => 'ASC',
-	)
-);
+	if ($posts) :
+	?>
 
-if ($posts) { ?>
-<section class="mou-site-wrap mou-site-wrap--padding wysiwyg u-soft-top-md u-push-bottom-lg">
-	<div>
-	<?php foreach ($posts as $post)	: ?>
-		<div class="o-grid__item u-1/2 u-1/4@md">
-			<h2><?php the_title(); ?></h2>
-			<?php if (get_field('surface')) {
-				$field = get_field_object('surface');
-				$value = get_field('surface');
-				$label = $field['choices'][$value]; ?>
-				<dl>
-					<dt><?php esc_html_e('Surface', 'moustache'); ?></dt>
-					<dd><?php esc_html_e($label); ?></dd>
-				</dl>
-			<?php } ?>
-
-			<?php /*
-			<?php if (get_field('image')) :
-				$image = get_field('image');
-			?>
-				<img src="<?php esc_attr_e($image); ?>" alt="">
-			<?php endif; ?>
-			*/ ?>
-
-			<?php if (get_field('address')) : $map = get_field('address'); ?>
-				<p><?php echo $map['address']; ?></p>
-					<?php /*
-					<div class="acf-map" data-zoom="24">
-					<div class="marker" data-lat="<?php echo esc_attr($map['lat']); ?>" data-lng="<?php echo esc_attr($map['lng']); ?>">
-				</div>
-				*/ ?>
-			<?php endif; ?>
-		</div>
+	<h2><?php esc_html_e('Baneoversikt', 'moustache'); ?></h2>
+	<ul class="u-soft-top-md">
+	<?php foreach ($posts as $post) : ?>
+		<li class="o-grid__item u-1/2 u-1/5@md">
+			<a href="/<?php echo ($post->post_name); ?>/">
+				<?php echo get_the_title($post->ID); ?>
+			</a>
+		</li>
 	<?php endforeach; ?>
-	</div>
-<?php } ?>
-</section>
+	</ul>
+	<?php endif; ?>
+</div>
 
 <?php get_footer(); ?>
