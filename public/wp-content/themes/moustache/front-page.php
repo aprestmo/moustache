787a04eccc364f91f3a53e528a3ed805a6c439ba
Alexander Prestmo <aprestmo@gmail.com>
2023-09-15 17:07:34 +0200

    Hide extra card title on front

--- public/wp-content/themes/moustache/front-page.php
+++ public/wp-content/themes/moustache/front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 
 <?php
 $args = array(
-  'posts_per_page' => 12,
+	'posts_per_page' => 12,
 );
 
 $query = new WP_Query($args);
@@ -20,85 +20,88 @@ $query = new WP_Query($args);
 if ($query->have_posts()) :
 ?>
 
-  <section class="c-cards o-section-md">
-    <?php
-    while ($query->have_posts()) :
-      $query->the_post();
-    ?>
+	<section class="c-cards o-section-md">
+		<?php
+		while ($query->have_posts()) :
+			$query->the_post();
+		?>
 
-      <article class="c-listing">
-        <a class="c-listing__link" href="<?php the_permalink(); ?>" title="">
-          <div class="c-listing__card">
+			<article class="c-listing">
+				<a class="c-listing__link" href="<?php the_permalink(); ?>" title="">
+					<div class="c-listing__card">
 
-            <?php
-            $image = get_the_post_thumbnail(null, 'reports-front', array('class' => 'c-listing__image'));
+						<?php
+						$image = get_the_post_thumbnail(null, 'reports-front', array('class' => 'c-listing__image'));
 
-            if ($image) :
-            ?>
-              <figure class="u-flush-bottom" role="group">
-                <?php echo $image; ?>
-              </figure>
-            <?php endif; ?>
+						if ($image) :
+						?>
+							<figure class="u-flush-bottom" role="group">
+								<?php echo $image; ?>
+							</figure>
+						<?php endif; ?>
 
-            <div class="u-soft-md">
-              <header class="c-listing__header">
-                <?php
-                $category = get_the_category();
+						<div class="u-soft-md">
+							<header class="c-listing__header">
+								<?php
+								$category = get_the_category();
 
-                if ($category) :
-                ?>
-                  <span class="c-listing__cat">
-                    <?php /* <i class="u-visually-hidden">IKON</i>&nbsp; */ ?>
-                    <?php echo esc_html($category[0]->cat_name); ?>
-                  </span>
-                <?php endif; ?>
+								if ($category) :
+								?>
+									<span class="c-listing__cat">
+										<?php /* <i class="u-visually-hidden">IKON</i>&nbsp; */ ?>
+										<?php echo esc_html($category[0]->cat_name); ?>
+									</span>
+								<?php endif; ?>
 
-                <?php $machine_date = get_the_date('Y-m-d'); ?>
-                <time class="c-listing__pubdate" datetime="<?php echo esc_attr($machine_date); ?>"><?php the_date('d.m.Y'); ?></time>
-              </header>
+								<?php $machine_date = get_the_date('Y-m-d'); ?>
+								<time class="c-listing__pubdate" datetime="<?php echo esc_attr($machine_date); ?>"><?php the_date('d.m.Y'); ?></time>
+							</header>
 
-              <?php if (!in_category('kamprapporter')) : ?>
-                <h2 class="c-listing__title"><?php the_title(); ?></h2>
-              <?php else : ?>
-                <h2 class="c-listing__title">
-                  <?php
-                    $str = get_the_title();
-                    $prefixed = str_starts_with($str, 'Kamprapport: ');
+							<?php if (!in_category('kamprapporter')) : ?>
+								<h2 class="c-listing__title"><?php the_title(); ?></h2>
+							<?php else : ?>
+								<h2 class="c-listing__title">
+									<?php
+										$str = get_the_title();
+										$prefixed = str_starts_with($str, 'Kamprapport: ');
 
-                    if ($prefixed) {
-                      $result = substr(strstr($str, " "), 1);
-                      echo $result;
-                    } else {
-                      the_title();
-                    }
-                  ?>
-                </h2>
-              <?php endif; ?>
-              <p>
-                <?php
-                $excerpt = get_the_excerpt();
-                $excerpt = substr($excerpt, 0, 500);
-                $result = substr($excerpt, 0, strrpos($excerpt, '.'));
-                echo $result . '...';
-                ?>
-              </p>
-            </div>
-          </div>
-        </a>
-      </article>
+										if ($prefixed) {
+											$result = substr(strstr($str, " "), 1);
+											echo $result;
+										} else {
+											the_title();
+										}
+									?>
+								</h2>
+							<?php endif; ?>
+							<p>
+								<?php echo get_the_excerpt();
+								// $excerpt = get_the_content();
+								// $excerpt = substr($excerpt, 0, 500);
+								// $result = substr($excerpt, 0, strrpos($excerpt, '.'));
+								// echo $result . '...';
+								// $excerpt = implode(' ', array_slice(explode(' ', get_the_content()), 0, 500));
+								// $result = substr($excerpt, 0, strrpos($excerpt, '.'));
+								// echo $result . '&hellip;';
+								?>
+							</p>
+						</div>
+					</div>
+				</a>
+			</article>
 
-    <?php endwhile; ?>
-    </section>
+		<?php endwhile; ?>
+		</section>
 
-    <!-- <nav class="o-section-sm u-push-bottom-lg u-text-center">
-      <a class="button" href="/arkiv/">Se hele arkivet</a>
-    </nav> -->
+		<!-- <nav class="o-section-sm u-push-bottom-lg u-text-center">
+			<a class="button" href="/arkiv/">Se hele arkivet</a>
+		</nav> -->
 <?php endif; ?>
 
 <div class="u-bg-white">
-  <div class="mou-site-wrap mou-site-wrap--padding">
-    <?php require get_template_directory() . '/template-parts/featured-players.php'; ?>
-  </div>
+	<div class="mou-site-wrap mou-site-wrap--padding">
+		<?php require get_template_directory() . '/template-parts/featured-players.php'; ?>
+	</div>
 </div>
 
 <?php get_footer(); ?>
--- public/wp-content/themes/moustache/functions.php
+++ public/wp-content/themes/moustache/functions.php
@@ -175,3 +175,30 @@ function vite_css_urls(string $entry): array
 
   return $urls;
 }
+
+function lt_html_excerpt($text) { // Fakes an excerpt if needed
+	global $post;
+	if ( '' == $text ) {
+			$text = get_the_content('');
+			$text = apply_filters('the_content', $text);
+			$text = str_replace('\]\]\>', ']]&gt;', $text);
+			/*just add all the tags you want to appear in the excerpt --
+			be sure there are no white spaces in the string of allowed tags */
+			$text = strip_tags($text,'<p><br><b><a><em><strong>');
+			/* you can also change the length of the excerpt here, if you want */
+			$excerpt_length = 50;
+			$words = explode(' ', $text, $excerpt_length + 1);
+			if (count($words)> $excerpt_length) {
+					array_pop($words);
+					array_push($words, '&hellip;');
+					$text = implode(' ', $words);
+			}
+	}
+	return $text;
+}
+
+/* remove the default filter */
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+
+/* now, add your own filter */
+add_filter('get_the_excerpt', 'lt_html_excerpt');
