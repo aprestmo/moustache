1e1a1173688147cef5dc52f8ec9267e2c7221bdd
Alexander Prestmo <aprestmo@gmail.com>
2023-09-28 09:39:25 +0200

    Add sub menus and escape to overlay

--- public/wp-content/themes/moustache/src/js/menu.js
+++ public/wp-content/themes/moustache/src/js/menu.js
@@ -5,9 +5,17 @@ const buttonClose = document.querySelector('#js-menu-toggle--close')
 const navigationPanel = document.querySelector('#js-site-navigation')
 
 buttonOpen.addEventListener('click', () => {
-  navigationPanel.classList.toggle('is-visible')
+	navigationPanel.classList.toggle('is-visible')
 })
 
 buttonClose.addEventListener('click', () => {
-  navigationPanel.classList.toggle('is-visible')
+	navigationPanel.classList.toggle('is-visible')
+})
+
+document.addEventListener('keyup', (e) => {
+	if (navigationPanel.classList.contains('is-visible')) {
+		if (e.key === 'Escape') {
+			navigationPanel.classList.toggle('is-visible')
+		}
+	}
 })
--- public/wp-content/themes/moustache/vite.config.js
+++ public/wp-content/themes/moustache/vite.config.js
@@ -10,41 +10,41 @@ const basePath = basename(__dirname)
 const themePath = `/wp-content/themes/${basePath}`
 
 export default defineConfig({
-  plugins: [
-    liveReload([`${__dirname}/*.php`, `${__dirname}/(lib|partials)/**/*.php`]),
-  ],
-  root: 'src',
-  base:
-    process.env.APP_ENV === 'development'
-      ? `${themePath}/src/`
-      : `${themePath}/dist/`,
-  resolve: {
-    alias: {
-      '@images': resolve(`${themePath}`, 'public/'),
-    },
-  },
-  build: {
-    // output dir for production build
-    outDir: resolve(__dirname, './dist'),
-    emptyOutDir: true,
+	plugins: [
+		liveReload([`${__dirname}/*.php`, `${__dirname}/(lib|partials)/**/*.php`]),
+	],
+	root: 'src',
+	base:
+		process.env.APP_ENV === 'development'
+			? `${themePath}/src/`
+			: `${themePath}/dist/`,
+	resolve: {
+		alias: {
+			'@images': resolve(`${themePath}`, 'public/'),
+		},
+	},
+	build: {
+		// output dir for production build
+		outDir: resolve(__dirname, './dist'),
+		emptyOutDir: true,
 
-    // emit manifest so PHP can find the hashed files
-    manifest: true,
+		// emit manifest so PHP can find the hashed files
+		manifest: true,
 
-    // esbuild target
-    target: 'modules',
+		// esbuild target
+		target: 'modules',
 
-    // our entry
-    rollupOptions: {
-      input: '/main.js',
-    },
-  },
-  server: {
-    // required to load scripts from custom host
-    cors: true,
+		// our entry
+		rollupOptions: {
+			input: '/main.js',
+		},
+	},
+	server: {
+		// required to load scripts from custom host
+		cors: true,
 
-    // we need a strict port to match on PHP side
-    strictPort: true,
-    port: 3005,
-  },
+		// we need a strict port to match on PHP side
+		strictPort: true,
+		port: 3005,
+	},
 })
