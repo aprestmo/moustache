850b51342a5325412542b4837f26cfbf7814c731
Alexander Prestmo <aprestmo@gmail.com>
2024-09-23 21:48:02 +0200

    Remove composer

--- public/wp-content/themes/moustache/footer.php
+++ public/wp-content/themes/moustache/footer.php
@@ -11,6 +11,40 @@
  */
 ?></main>
 
+<?php
+require_once 'vendor/autoload.php';
+
+// Set the default environment file to production
+$envFile = '.env.production';
+
+// Check if NODE_ENV is set in $_ENV or $_SERVER and adjust the .env file accordingly
+if (file_exists(__DIR__ . '/.env.development') && ($_ENV['NODE_ENV'] ?? $_SERVER['NODE_ENV']) === 'development') {
+	$envFile = '.env.development';
+} elseif (file_exists(__DIR__ . '/.env.production') && ($_ENV['NODE_ENV'] ?? $_SERVER['NODE_ENV']) === 'production') {
+	$envFile = '.env.production';
+}
+
+// Load the determined .env file
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, $envFile);
+$dotenv->load();
+
+// Debugging output to verify the correct environment file is loaded
+echo 'Loaded Environment File: ' . $envFile . '<br>';
+echo '$_ENV NODE_ENV: ' . var_export($_ENV['NODE_ENV'] ?? 'not set', true) . '<br>';
+echo '$_SERVER NODE_ENV: ' . var_export($_SERVER['NODE_ENV'] ?? 'not set', true) . '<br>';
+
+// Set $nodeEnv based on the values found
+$nodeEnv = $_ENV['NODE_ENV'] ?? $_SERVER['NODE_ENV'] ?? null;
+echo 'NODE_ENV: ' . var_export($nodeEnv, true) . '<br>';
+
+// Determine if the environment is in development mode
+$isDevMode = $nodeEnv === 'development';
+var_dump($isDevMode); // Should output true if in development mode
+
+// Set the base URL for assets
+$baseURL = $isDevMode ? '/public/' : '/dist/assets/';
+?>
+
 <footer class="c-footer" role="contentinfo">
 	<div class="mou-site-wrap mou-site-wrap--padding">
 		<div class="o-grid o-section-md">
