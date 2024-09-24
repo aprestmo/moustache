460fe96e46f595d62bba684000752abafb75b7aa
Alexander Prestmo <aprestmo@gmail.com>
2024-09-24 14:58:43 +0200

    Add hmr

--- public/wp-content/themes/moustache/vite.config.js
+++ public/wp-content/themes/moustache/vite.config.js
@@ -5,12 +5,24 @@ const THEME_NAME = 'moustache';
 
 export default defineConfig(({ mode }) => ({
   plugins: [liveReload(['./**/*.php'])],
-  server: {
+  // server: {
+  //   hmr: {
+  //     host: 'localhost',
+  //   },
+  // },
+	server: {
+    host: '0.0.0.0', // Allows access from any network interface, including kampbart.local
+    port: 5173, // Default Vite port, adjust if necessary
+    cors: {
+      origin: 'http://kampbart.local', // Allow requests from kampbart.local
+      credentials: true, // Allows cookies to be sent
+    },
     hmr: {
-      host: 'localhost',
+      host: 'kampbart.local', // Set the HMR (Hot Module Replacement) host to match kampbart.local
+      port: 5173, // Ensure the port matches your Vite server port
     },
   },
-  base: mode === 'production' ? `/wp-content/themes/${THEME_NAME}/dist/` : '/',
+  base: mode === 'production' ? `/wp-content/themes/${THEME_NAME}/dist/` : `/wp-content/themes/${THEME_NAME}/src/`,
   build: {
 		manifest: true,
     outDir: 'dist',
