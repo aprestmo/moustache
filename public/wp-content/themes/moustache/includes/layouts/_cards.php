5d4a001c30f03650e5b9047b5aa8763f7885ae4f
Alexander Prestmo <aprestmo@gmail.com>
2024-11-16 08:49:46 +0100

    Refactor cards

--- public/wp-content/themes/moustache/includes/layouts/_cards.php
+++ public/wp-content/themes/moustache/includes/layouts/_cards.php
@@ -1,41 +1,100 @@
 <?php
 
-// Check for yellow cards
-// @TODO: Check if the same player has got two yellow cards, and send him off
+/**
+ * Display cards for a match
+ *
+ * @package Moustache
+ */
 
-function cards()
+/**
+ * Display cards for a specific half
+ *
+ * @param string $half Which half ('first' or 'second')
+ * @return void
+ */
+function display_half_cards(string $half): void
 {
-	if (have_rows('cards_first_half')):
-		echo '<strong>';
-		esc_html_e('Card in 1st half', 'moustache');
-		echo '</strong>';
-		echo '<ul class="c-report u-soft-bottom-md">';
-		while (have_rows('cards_first_half')) : the_row();
-			$player = get_sub_field('card_player_first_half');
-			$colour = get_sub_field('card_colour_first_half');
-			echo '<li data-card="' . $colour . '">';
-			echo '<a href="/spiller/' . $player->post_name . '">';
-			esc_html_e($player->post_title);
-			echo '</a>';
-			echo '</li>';
-		endwhile;
-		echo '</ul>';
-	endif;
+	$field_name = "cards_{$half}_half";
+	$player_field = "card_player_{$half}_half";
+	$colour_field = "card_colour_{$half}_half";
 
-	if (have_rows('cards_second_half')):
-		echo '<strong>';
-		esc_html_e('Card in 2nd half', 'moustache');
-		echo '</strong>';
-		echo '<ul class="c-report u-soft-bottom-md">';
-		while (have_rows('cards_second_half')) : the_row();
-			$player = get_sub_field('card_player_second_half');
-			$colour = get_sub_field('card_colour_second_half');
-			echo '<li data-card="' . $colour . '">';
-			echo '<a href="/spiller/' . $player->post_name . '">';
-			esc_html_e($player->post_title);
-			echo '</a>';
-			echo '</li>';
-		endwhile;
-		echo '</ul>';
+	if (have_rows($field_name)) :
+?>
+		<strong>
+			<?php
+			printf(
+				esc_html__('Card in %s half', 'moustache'),
+				$half === 'first' ? '1st' : '2nd'
+			);
+			?>
+		</strong>
+		<ul class="c-report u-soft-bottom-md">
+			<?php
+			while (have_rows($field_name)) :
+				the_row();
+				$player = get_sub_field($player_field);
+				$colour = get_sub_field($colour_field);
+
+				if ($player) : ?>
+					<li data-card="<?php echo esc_attr($colour); ?>">
+						<a href="<?php echo esc_url(get_permalink($player->ID)); ?>">
+							<?php echo esc_html($player->post_title); ?>
+						</a>
+					</li>
+			<?php
+				endif;
+			endwhile;
+			?>
+		</ul>
+	<?php
 	endif;
+}
+
+/**
+ * Display cards from the 'cards' field
+ *
+ * @return void
+ */
+function display_additional_cards(): void
+{
+	if (have_rows('cards')) :
+	?>
+		<strong><?php esc_html_e('Cards', 'moustache'); ?></strong>
+		<ul class="c-report u-soft-bottom-md">
+			<?php
+			while (have_rows('cards')) :
+				the_row();
+				$player = get_sub_field('card_player');
+				$colour = get_sub_field('card_colour');
+
+				if ($player) : ?>
+					<li data-card="<?php echo esc_attr($colour); ?>">
+						<a href="<?php echo esc_url(get_permalink($player->ID)); ?>">
+							<?php echo esc_html($player->post_title); ?>
+						</a>
+					</li>
+			<?php
+				endif;
+			endwhile;
+			?>
+		</ul>
+<?php
+	endif;
+}
+
+/**
+ * Display all cards for a match
+ *
+ * @return void
+ */
+function cards(): void
+{
+	// Sjekk om det finnes data i 'cards' feltet
+	if (have_rows('cards')) {
+		display_additional_cards();
+	} else {
+		// Hvis ikke, vis kort fra første og andre omgang
+		display_half_cards('first');
+		display_half_cards('second');
+	}
 }
