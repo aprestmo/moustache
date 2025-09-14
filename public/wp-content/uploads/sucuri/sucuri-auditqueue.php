86d14e23e348df48fda0e1c0ce368ca8427dd91f
Alexander Prestmo <aprestmo@gmail.com>
2025-09-14 23:07:25 +0200

    Rollback

--- public/wp-content/uploads/sucuri/sucuri-auditqueue.php
+++
@@ -1,7 +0,0 @@
-<?php
-// datastore=auditqueue;
-// created_on=1688455571;
-// updated_on=1688455571;
-exit(0);
-?>
-1688455571_3762:"Debug: utvikler, 178.232.229.239; Sucuri plugin has been uninstalled"
--- public/wp-content/uploads/sucuri/sucuri-ignorescanning.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=ignorescanning;
-// created_on=1688455571;
-// updated_on=1688455571;
-exit(0);
-?>
